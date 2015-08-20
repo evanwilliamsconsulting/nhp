5a494e3078c9460f09fe2c3c52c725fe40b4422c
Evan Jan Williams <editor@newhollandpress.com>
2015-08-20 23:36:35 +0000

    fixed Controller route mappings

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -82,7 +82,7 @@ return array(
             'Application\Controller\Headline' => 'Application\Controller\HeadlineController',
             'Application\Controller\Pix' => 'Application\Controller\PixController',
             'Application\Controller\PixLink' => 'Application\Controller\PixlinkController',
-            'Application\Controller\Wordage' => 'Application\Controller\VerbageController',
+            'Application\Controller\Wordage' => 'Application\Controller\WordageController',
             'Application\Controller\Article' => 'Application\Controller\ArticleController',
         ),
     ),
