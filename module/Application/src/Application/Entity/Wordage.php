fb43e5e19fb3fed6c9c6ec5d551f9c1dc99e0466
Evan Jan Williams <editor@newhollandpress.com>
2015-08-24 08:53:56 +0000

    Wordage ready to retrieve data

--- module/Application/src/Application/Entity/Wordage.php
+++ module/Application/src/Application/Entity/Wordage.php
@@ -1,8 +1,12 @@
 <?php
 
-
+namespace Application\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
 
 /**
  * Wordage
@@ -10,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="Wordage")
  * @ORM\Entity
  */
-class Wordage
+class Wordage implements InputFilterAwareInterface
 {
     /**
      * @var integer
@@ -28,7 +32,7 @@ class Wordage
      */
     private $wordage;
 
-    public getWordage()
+    public function getWordage()
     {
         return $this->wordage;
     }
@@ -42,17 +46,58 @@ class Wordage
      *
      * @ORM\Column(name="columnSize", type="integer", nullable=false)
      */
-    private $columnsize;
+    private $columnSize;
 
-    public function getColumnsize()
+    public function getColumnSize()
     {
-        return $this->columnsize;
+        return $this->columnSize;
     }
 
-    public function setColumnsize($value)
+    public function setColumnSize($value)
     {
-        $this->columnsize = $value;
+        $this->columnSize = $value;
     }
 
+    protected $inputFilter;
 
+    public function exchangeArray($data)
+    {
+        $this->id = (isset($data['id'])) ? $data['id'] : null;
+        $this->wordage = (isset($data['wordage'])) ? $data['wordage'] : null;
+        $this->columnSize = (isset($data['columnSize'])) ? $data['columnSize'] : null;
+    }
+    
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+    
+
+            $inputFilter->add(array(
+                'name' => 'id',
+                'required' => false,
+	    ));
+
+            $inputFilter->add(array(
+                'name' => 'wordage',
+                'required' => false,
+	    ));
+            $inputFilter->add(array(
+                'name' => 'columnSize',
+                'required' => false,
+	    ));
+
+	   $this->inputFilter = $inputFilter;
+        }
+       
+	return $this->inputFilter;
+    }
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not Used");
+    }
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
 }
