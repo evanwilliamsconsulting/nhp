ff8152da55e5ecf9d248f15ac75dba74d4f59c98
newhollandpress <editor@newhollandpress.com>
2015-09-07 21:11:19 +0000

    Welcome Helper

---
+++ module/Application/src/Application/View/Helper/Welcome.php
@@ -0,0 +1,34 @@
+<?php
+namespace Application\View\Helper;
+use Zend\View\Helper\AbstractHelper;
+use Zend\Session\Container;
+ 
+class Welcome extends AbstractHelper
+{
+    protected static $state;
+
+    public function __invoke()
+    {
+	$userSession = new Container('user');
+        if (!isset($userSession->loggedin))
+        {
+            return "<a href='http://www.newhollandpress.com/auth/login'>Login</a>";
+	}
+        else
+        {
+	    $username = $userSession->username;
+	    $retval = "Welcome&nbsp;" . $username;
+	    $retval .= "&nbsp;<a href='http://www.newhollandpress.com/auth/login/logout'>Logout</a>";
+	    return $retval;
+        }
+    }
+    public function setState()
+    {
+        $this->state = true;
+    }
+    public function clearState()
+    {
+        $this->state = false;
+    } 
+}
+?>
