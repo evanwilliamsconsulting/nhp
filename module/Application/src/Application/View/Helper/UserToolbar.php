c982d1bcf323e90d03eeae4a65669eaafbef4937
newhollandpress <editor@newhollandpress.com>
2015-10-13 05:05:27 +0000

    Add UserToolbar but do not remove Welcome toolbar yet

---
+++ module/Application/src/Application/View/Helper/UserToolbar.php
@@ -0,0 +1,47 @@
+<?php
+namespace Application\View\Helper;
+use Zend\View\Helper\AbstractHelper;
+use Zend\Session\Container;
+ 
+class UserToolbar extends AbstractHelper
+{
+    protected static $state;
+
+    public function __invoke()
+    {
+    	$userToolbarEMPTY = "<div id='user_toolbar' class='toolbar'>";
+    	$userToolbarHTML = "<div id='user_toolbar' class='toolbar'>";
+		$userToolbarHTML .= "<ul>";
+		$userSession = new Container('user');
+        if (!isset($userSession->loggedin))
+        {
+			$userToolbarHTML .= "<li onclick=\"";
+			$userToolbarHTML .= "clickLogin();";
+			$userToolbarHTML .= "\">";
+			$userToolbarHTML .= "Login";
+			$userToolbarHTML .= "</li>";
+			// Old Action
+            // return "<a href='http://www.newhollandpress.com/auth/login'>Login</a>";
+		}
+		else 
+		{
+	        $username = $userSession->username;
+	        //$retval = "Welcome&nbsp;" . $username;
+	        //$retval .= "&nbsp;<a href='http://www.newhollandpress.com/auth/login/logout'>Logout</a>";
+	        //return $retval;
+        }
+		$userToolbarHTML .= "</ul>";
+		$userToolbarHTML .= "</div>";
+		$userToolbarEMPTY .= "</div>";
+		return $userToolbarEMPTY;
+    }
+    public function setState()
+    {
+        $this->state = true;
+    }
+    public function clearState()
+    {
+        $this->state = false;
+    } 
+}
+?>
