ab27ed6460fe1d98e2dd4f3023f8a6c731b21ae5
newhollandpress <editor@newhollandpress.com>
2015-10-13 05:02:26 +0000

    first javascript file for window and form behavoir

---
+++ public/js/nhpress.js
@@ -0,0 +1,22 @@
+$(document).ready(function()
+{
+	window_info = ({ "width" : $(window).width(), "height" : $(window).height()});
+	$.ajax({
+		type:"POST",
+		data:window_info,
+		url:"index/setsize",
+		success:function(data) {
+			//alert(data);
+		}
+	});
+	clickLogin = function()
+	{
+		$.ajax({
+			type:"POST",
+			url:"auth/login",
+			success: function(data) {
+				$("#loginBox").html(data);
+			}
+		});
+	}
+});
