74cdd621566de0a5355edf7ef4904c7e99f4c8c6
newhollandpress <editor@newhollandpress.com>
2015-11-07 23:53:16 +0000

    new Wordage Helper

--- module/Application/src/Application/View/Helper/WordageHelper.php
+++ module/Application/src/Application/View/Helper/WordageHelper.php
@@ -3,14 +3,54 @@ namespace Application\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
 use Zend\Session\Container;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;  
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\View\Renderer\PhpRenderer;
+use Zend\View\Model\ViewModel;
+use Application\Service\WordageService as WordageService;  
  
-class WordageHelper extends AbstractHelper
+class WordageHelper extends AbstractHelper implements ServiceLocatorAwareInterface
 {
     protected static $state;
 	protected $wordageObject;
 	protected $username;
 	protected $itemId;
+	protected $viewmodel;
+	protected $renderer;
+	protected $wordageService;
 
+    /** 
+     * Set the service locator. 
+     * 
+     * @ param ServiceLocatorInterface $serviceLocator 
+     * @ return CustomHelper 
+     */ 
+    public function __construct(WordageService $wordageService)
+	{
+		$this->wordageService = $wordageService;
+	}
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)  
+    {  
+        $this->serviceLocator = $serviceLocator;  
+        return $this;  
+    } 
+    /** 
+     * Get the service locator. 
+     * 
+     * @ return \Zend\ServiceManager\ServiceLocatorInterface 
+     */  
+    public function getServiceLocator()  
+    {  
+        return $this->serviceLocator;  
+    }  
+    public function setViewModel(ViewModel $viewmodel)
+	{
+		$this->viewmodel = $viewmodel;
+	}
+	public function getViewModel()
+	{
+		return $this->viewmodel;
+	}
 	public function setWordageObject($wordageObject)
 	{
 		$this->wordageObject = $wordageObject;
@@ -23,27 +63,33 @@ class WordageHelper extends AbstractHelper
 	{
 		$this->itemId = $itemId;
 	}
+	public function setRenderer($renderer)
+	{
+		$this->renderer = $renderer;
+	}
+	public function getRenderer()
+	{
+		return $this->renderer;
+	}
     public function __invoke()
     {
-    	$wordageObject = $this->wordageObject;
-		$base = 'https://newhollandpress.com/wordage/view/';
-		$title = $wordageObject->getTitle();
-		$id = $wordageObject->getId();
-		$output = "<div id='" . $this->itemId . "' class='itemHelper_Wide'>";
-		$output .= "<ul>";
-		$output .= "<li>";
-		$output .= "<span>Wordage</span>";
-		$output .= "</li>";
-		$output .= "<li>";
-		$output .= "<a href='" . $base . "'>" . $title . "</a>";
-		$output .= "</li>";
-		$output .= "<li>";
-		$output .= $wordageObject->getWordage();
-		$output .= "</li>";
-		$output .= "</ul>";
-		$output .= "</div>";
+    	//$sm = $this->getServiceLocator()->getServiceLocator();  
+        //$config = $sm->get('application')->getConfig(); 
+ 
+        $retval = "<div>";
+		$retval .= "Wordage Helper";
+		$retval .= print_r($this->wordageService->findAllWordage(),true);
+		$retval .= "</div>";
+    	
+    	//$view = $this->getViewModel();
 		
-		return $output;
+		//$view->setTemplate('items/wordage.phtml');
+		
+		//return $view;
+		
+		return $retval;
+		
+		//return print_r($view,true);
     }
 }
 ?>
